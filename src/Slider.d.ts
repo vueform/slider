40f5ae16abba238499a5c21192afc7aa051182f9
Adam Berecz <adam@laraform.io>
2021-06-24 13:22:21 +0200

    added ts support

---
+++ src/Slider.d.ts
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+
+declare class Slider extends Vue {
+  modelValue?: any;
+  value?: any;
+  id?: string;
+  disabled?: boolean;
+  min?: number;
+  max?: number;
+  step?: number;
+  orientation?: 'horizontal'|'vertical';
+  direction?: 'ltr'|'rtl';
+  tooltips?: boolean;
+  options?: object;
+  merge?: number;
+  format?: any;
+  classes?: object;
+
+  $emit(eventName: 'change', e: {originalEvent: Event, value: any}): this;
+  $emit(eventName: 'update', e: {originalEvent: Event, value: any}): this;
+}
+
+export default Slider;
